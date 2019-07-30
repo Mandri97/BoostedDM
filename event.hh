9f00f23ab2a37f2cbbc18efea54cdc19a89fd4df
Mandri97 <manoa.andriamirado@gmail.com>
2019-07-30 11:32:22 +0300

    First commit

---
+++ event.hh
@@ -0,0 +1,66 @@
+#include <vector>
+#include <assert.h>
+
+#ifndef __EVENT_H_
+#define __EVENT_H_
+
+/* Structure to describe a pulse */
+
+struct Pulse_t {
+    int segment,
+        PID;
+
+    float height,
+          energy,
+          time,
+          PSD;
+};
+
+/* class for one event */
+class Event {
+    private:
+        std::vector<Pulse_t> pulses;     // vector containing all pulses in one event
+        float energyEvent;               // energy of the event
+
+        bool containNeutronRecoil,
+             containNLiCapture;
+
+    public:
+        Event();
+        ~Event();
+
+        void Initialize();
+
+        // Getter
+        int      getNumberOfPulses();
+        float    getEnergyEvent();
+        Pulse_t* getPulse(int iPulse);
+
+        // Setter
+        void addPulse(Pulse_t pulse);
+
+        int isSinglePulse();
+        bool isContainingNeutronRecoil();
+        bool isContainingNLiCapture();
+};
+
+class Events {
+    private:
+        std::vector<Event> events;
+
+    public:
+        Events();
+        ~Events();
+
+        void Initialize();
+
+        // Getter
+        int    getNumberOfEvents();
+        Event* getEvent(int iEvent);
+
+
+        // Setter
+        void addEvent(Event event);
+};
+
+#endif 
