b86cd9835acdb80800d359687893b1fb4c7e73b3
Mandri97 <manoa.andriamirado@gmail.com>
2019-11-14 00:57:54 -0600

    Run time

--- totalRunningTime.C
+++ totalRunningTime.C
@@ -1,7 +1,16 @@
 #include <TFile.h>
 #include <TVectorT.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-void totalRunningTime(char *fileName){
+using namespace std;
+
+
+double runingTime = 0.0;
+
+
+void totalRunningTime(string fileName){
     auto _file = new TFile(fileName);
 
     runingTime += ((TVectorT<double>*) _file->Get("runtime"))->Max();
@@ -10,7 +19,12 @@ void totalRunningTime(char *fileName){
 }
 
 int main(int argc, char *argv[]){
-    double runingTime = 0.0;
+    ifstream _textFile(argv[1]);
+
+    string file;
+
+    while (_textFile >> file) totalRunningTime(file);
 
     cout << "Total running time: " << runingTime << " s, " << runingTime / (60 * 60) << " h" << endl;    
+    _textFile.close();
 }
