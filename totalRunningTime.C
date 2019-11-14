5c46c90b9ce8d3d2682e20903828e34ea18aa61e
Mandri97 <manoa.andriamirado@gmail.com>
2019-11-14 00:48:39 -0600

    Run time

---
+++ totalRunningTime.C
@@ -0,0 +1,13 @@
+void totalRunningTime(char *fileName){
+    auto _file = new TFile(fileName);
+
+    runingTime += ((TVectorT<double>*) _file->Get("runtime"))->Max();
+
+    _file->Close();
+}
+
+int main(int argc, char *argv[]){
+    double runingTime = 0.0;
+
+    cout << "Total running time: " << runingTime << " s, " << runingTime / (60 * 60) << " h" << endl;    
+}
