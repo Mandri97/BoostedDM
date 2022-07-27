c7c447176da8883665e923fcea5e28d9d1f29efd
Manoa Andriamirado <manoa@vejur.phys.iit.edu>
2022-07-27 15:46:44 -0500

    Add scripts for Jonte's muon analysis

---
+++ plotRate.C
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+
+#include <TH1F.h>
+#include <TFile.h>
+#include <TGraph.h>
+#include <TCanvas.h>
+#include <TVectorT.h>
+
+using namespace std;
+
+void helper(){
+    printf("Usage: ./plotRate listOfAnalyzedFiles.txt\n");
+    exit(-1);
+}
+
+void FILE_NOT_FOUND(const char *filename){
+    printf("Could not find %s, exiting the program.\n", filename);
+    exit(-1);
+}
+
+void plotRate(char* filename){
+    // Create a graph 
+    TGraph *graph = new TGraph();
+    
+    int fileCounter = 1;
+
+    // Open txt file
+    ifstream _inFile;
+    _inFile.open(filename);
+    if (!_inFile.is_open()) FILE_NOT_FOUND(filename);
+
+    string line;
+    while (getline(_inFile, line)){
+        // Open rootfile
+        TFile* _rootfile = new TFile(line.c_str());
+        if (_rootfile->IsZombie()) FILE_NOT_FOUND(line.c_str());
+
+        // Get histogram
+        auto hEnergySelectedEvents = (TH1D*)_rootfile->Get("hEnergyPerEvent");
+
+        // Get runtime
+        double runtime  = ((TVectorT<double> *) _rootfile->Get("runtime"))->Max();
+        double binWidth = hEnergySelectedEvents->GetBinWidth(1);
+        double aliveSeg = 126;
+
+        double scaling = 1 / (runtime * binWidth * aliveSeg * 24.2);
+
+        hEnergySelectedEvents->Scale(scaling);
+
+        // Integration start and end energy
+        double startEnergy = 15;
+        double endEnergy = 50;
+
+        // Calculate the integration bin
+        int binMin = hEnergySelectedEvents->GetXaxis()->FindBin(startEnergy);
+        int binMax = hEnergySelectedEvents->GetXaxis()->FindBin(endEnergy);
+        binMax = (binMax > hEnergySelectedEvents->GetNbinsX()) ? hEnergySelectedEvents->GetNbinsX() : binMax;
+
+        // Integrate
+        double rate = hEnergySelectedEvents->Integral(binMin, binMax, "width");
+
+        graph->SetPoint(fileCounter -1, fileCounter, rate);
+        
+        fileCounter++;
+
+        // Close file to avoid memory leak
+        _rootfile->Close();
+    }
+    // Free memory
+    _inFile.close();
+
+    TCanvas *c = new TCanvas("c", "", 800, 500);
+    graph->Draw();
+    c->SaveAs("RateMuon.pdf");
+}
+
+int main(int argc, char* argv[]){
+    // Check the number of argument
+    if (argc != 2) helper();
+
+    plotRate(argv[1]);
+}
---
+++ runAnalysis.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+function readTxtFile() {
+	counter=0
+	FILELIST=$1
+
+    # TODO: Specify where the PBS script is
+    PBS_SCRIPT=$HOME/work/BoostedDM/FinalPlot/runAnalysis.pbs
+
+	while read CMD; do
+        echo "Submiting job $counter - Analysis of $CMD ..."
+
+		qsub -F "$CMD $2" $PBS_SCRIPT
+		counter=$((counter + 1))
+
+		if (( $counter % 11 == 0)); then
+			sleep 200
+			counter=0
+		fi
+	done < "$FILELIST"
+}
+
+
+function checkLiveTime(){
+# Create script to launch ROOT command
+SCRIPT="/tmp/readRunTime.C"
+
+cat > ${SCRIPT}<< EOF
+//------------FILE-CONTENT------------//
+
+void readRunTime(const char* filename ){
+    TFile *_file = new TFile(filename);
+	
+	auto runtime = (TVectorT<double>*) _file->Get("runtime");
+
+	if (runtime->GetNrows() != 1) {
+		printf("Multiple runtime? Seriously?");
+		exit(1);
+	}
+
+	std::cout << std::setprecision(20) << runtime->Max() << std::endl;
+
+    _file->Close();
+}
+
+//------------FILE-CONTENT------------//
+EOF
+
+	touch $2/runtimeCheck.txt
+
+    FILELIST=$1
+
+	while read FILE; do
+        echo "Reading $FILE ..."
+
+		CMD="root -l -q '/tmp/readRunTime.C(\"$FILE\")'"	
+		echo $(eval $CMD | tail -1) >> $2/runtimeCheck.txt
+	done < "$FILELIST"
+
+	rm -rf /tmp/readRunTime.C
+}
+
+################################
+#         Main Program         #
+################################
+# Check the right number of args
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./runAnalysis.sh listOfFilesToAnalyze.txt path/to/output_dir"
+    exit 1
+fi
+
+# Prompt to get user's choice of analysis
+CONF_="n"
+ANALYSIS_=10
+
+while [ "$CONF_" != "y" ]; do
+    echo "What kind of analysis would you like to do?"
+    echo "(1) - Analyzing files"
+    echo "(2) - Get runtime"
+    read ANALYSIS_
+    echo "Confirming ($ANALYSIS_) based analysis ? (y/n)"
+    read CONF_
+done
+
+LISTOFFILES=$1
+
+# Calling function
+if [ $ANALYSIS_ -eq 1 ]; then
+    readTxtFile "${LISTOFFILES}" $2
+elif [ $ANALYSIS_ -eq 2 ]; then
+    # Overwrite outputfile
+    rm -f $2/runtimeCheck.txt
+
+    checkLiveTime "${LISTOFFILES}" $2
+fi
+
